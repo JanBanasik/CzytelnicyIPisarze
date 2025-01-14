69f296d50b1bb17c092405bcab10484ab7fff9fd
JanBanasik <jan.jerzy.banasik@gmail.com>
2025-01-15 00:52:25 +0100

    Poprawne logowanie

--- model/src/main/java/pl/edu/agh/kis/pz1/Library.java
+++ model/src/main/java/pl/edu/agh/kis/pz1/Library.java
@@ -16,9 +16,12 @@ class Library {
     public synchronized void requestRead(int readerId) throws InterruptedException {
         String currentReaderSymbol = "Reader " + readerId;
         System.out.println(currentReaderSymbol +  " chce czytać");
+        System.out.println("---------------------------");
 
         waitingPeople.add(currentReaderSymbol);
         dumpQueue(waitingPeople);
+        dumpList(peopleInside);
+        System.out.println("---------------------------");
         while(!Objects.equals(waitingPeople.peek(), currentReaderSymbol) || peopleCurrentlyInside == maxPeopleInside) {
             wait();
         }
@@ -26,7 +29,10 @@ class Library {
         peopleCurrentlyInside++;
         peopleInside.add(currentReaderSymbol);
         System.out.println(currentReaderSymbol + " wszedl i czyta");
+        System.out.println("---------------------------");
+        dumpQueue(waitingPeople);
         dumpList(peopleInside);
+        System.out.println("---------------------------");
 
     }
 
@@ -34,7 +40,11 @@ class Library {
         String currentReaderSymbol = "Reader " + readerId;
         peopleCurrentlyInside--;
         peopleInside.remove(currentReaderSymbol);
-        System.out.println(currentReaderSymbol + " zakonczyl pisanie");
+        System.out.println(currentReaderSymbol + " zakonczyl czytanie");
+        System.out.println("---------------------------");
+        dumpQueue(waitingPeople);
+        dumpList(peopleInside);
+        System.out.println("---------------------------");
         notifyAll();
     }
 
@@ -42,24 +52,33 @@ class Library {
     public synchronized void requestWrite(int writerId) throws InterruptedException {
         String currentWriterSymbol = "Writer " + writerId;
         System.out.println(currentWriterSymbol +  " chce pisać");
+        System.out.println("---------------------------");
 
         waitingPeople.add(currentWriterSymbol);
         dumpQueue(waitingPeople);
+        dumpList(peopleInside);
+        System.out.println("---------------------------");
         while(!Objects.equals(waitingPeople.peek(), currentWriterSymbol) || peopleCurrentlyInside > 0) {
             wait();
         }
         waitingPeople.poll();
         peopleCurrentlyInside = maxPeopleInside;
         peopleInside.add(currentWriterSymbol);
-        System.out.println(currentWriterSymbol + " pisze");
+        System.out.println(currentWriterSymbol + " wszedl i pisze");
+        System.out.println("---------------------------");
+        dumpQueue(waitingPeople);
         dumpList(peopleInside);
+        System.out.println("---------------------------");
     }
 
     public synchronized void finishWrite(int writerId) {
         String currentWriterSymbol = "Writer " + writerId;
         peopleCurrentlyInside = 0;
         peopleInside.remove(currentWriterSymbol);
-        System.out.println(currentWriterSymbol + "zakonczyl czytanie");
+        System.out.println(currentWriterSymbol + " zakonczyl pisanie");
+        System.out.println("---------------------------");
+        dumpQueue(waitingPeople);
+        dumpList(peopleInside);
         notifyAll();
     }
 
